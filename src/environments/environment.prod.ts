191ad02ce5ae2dd7e6e3b12d702c77bcc7a02c96
shwethaveeraiah <40417516+shwethaveeraiah@users.noreply.github.com>
2018-06-21 16:54:51 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+   config: {
+    tenant: 'mindtreeonline.onmicrosoft.com',
+    clientId: 'ee8743a1-be79-4cfe-8536-97c88acfbb49'
+  }
 };
